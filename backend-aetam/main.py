01b90ec0089197ae989972ce7685360520057557
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-22 01:09:55 +0000

    #18 Update file structure

--- backend-aetam/main.py
+++ backend-aetam/main.py
@@ -1,10 +1,5 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello, world!'
+from aetam import app
 
 if __name__ == '__main__':
     host = '0.0.0.0'
