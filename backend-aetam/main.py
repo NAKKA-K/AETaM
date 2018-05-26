7cd7dd3868a97a5d0063aa350be2368de1d1ff81
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-26 05:00:03 +0000

    #25 Update use config

--- backend-aetam/main.py
+++ backend-aetam/main.py
@@ -2,6 +2,4 @@ from flask import Flask
 from aetam import app
 
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = '8000'
-    app.run(host=host, port=port)
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
