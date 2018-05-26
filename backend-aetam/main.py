0467bc25a2236edec441948c76bc7b5283a0cdc3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-26 13:44:49 +0000

    #25 Add command to main

--- backend-aetam/main.py
+++ backend-aetam/main.py
@@ -1,5 +1,19 @@
+import sys
+import os
 from flask import Flask
 from aetam import app
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    if len(sys.argv) == 1:
+        print("> 引数が存在しません\n")
+        os.system('make help')
+        sys.exit()
+
+    if sys.argv[1] == 'runserver':
+        app.run(host=app.config['HOST'], port=app.config['PORT'])
+    elif sys.argv[1] == 'migrate':
+        from aetam.models import util_db
+        util_db.init()
+    else:
+        print('存在しないコマンドです')
+
