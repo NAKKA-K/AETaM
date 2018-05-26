4fafe8e60e4124f9700ef8f92527bb2806e4a0bd
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-26 13:43:26 +0000

    #25 Update class to module

--- backend-aetam/aetam/models/util_db.py
+++ backend-aetam/aetam/models/util_db.py
@@ -1,21 +1,31 @@
+import os
+import sqlite3
 from flask import g
 from aetam import app
 from contextlib import closing
 
-class UtilDB(object):
-    def init(self):
-        with closing(self.connect()) as db:
-            with app.open_resource('schema.sql') as :
-                pass
+# DBファイルの作成を行う
+# schemaはmigrationsディレクトリに格納
+def init():
+    with closing(connect()) as db:
+        schema_file = os.path.join(app.config['BASE_DIR'], 'aetam/migrations/schema.sql')
+        with app.open_resource(schema_file) as sql:
+            db.cursor().executescript(sql.read().decode('utf-8'))
+        db.commit()
 
-    def connect(self):
-        return sqlite3.connect(app.config['DATABASE'])
+# DBへの接続変数を返す
+def connect():
+    return sqlite3.connect(app.config['DATABASE'])
 
-    @app.before_request
-    def before_request(self):
-        g.db = self.connect
+# DBへの接続変数をgという環境変数に格納する
+# appのリクエスト直前に呼ばれる
+@app.before_request
+def before_request():
+    g.db = connect()
 
-    @app.after_request
-    def after_request(self, response):
-        g.db.close()
-        return response
+# DBの接続変数を閉じる
+# appのリクエスト終了時に呼ばれる
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
