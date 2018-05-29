9450d0fa1943191ad99d562289fae77ad1793dfe
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-29 08:19:48 +0000

    #31 Add login api. Return json data

--- backend-aetam/aetam/views/login.py
+++ backend-aetam/aetam/views/login.py
@@ -5,6 +5,8 @@ from flask import request
 from flask import flash
 from flask import url_for
 from flask import session
+from flask import Response
+from flask import json
 
 
 class LoginView(MethodView):
@@ -13,14 +15,34 @@ class LoginView(MethodView):
 
     def post(self):
         error = None
-        if request.form['username'] == "":
-            error = 'Invalid username'
-        elif request.form['password'] == "":
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('Logged in!')
-            return redirect(url_for('index'))
+        if request.headers['Content-Type'] == 'application/json':
+            data = {}
+            if request.json['username'] == "":
+                data['error'] = 'Invalid username'
+            elif request.json['password'] == "":
+                data['error'] = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                data['message'] = "Logged in!"
+                return Response(
+                    status=200,
+                    response=json.dumps(data),
+                    mimetype='application/json'
+                )
+            return Response(
+                status=400,
+                response=json.dumps(data),
+                mimetype='application/json'
+            )
+        else: # TODO: 開発テスト段階が終われば削除する
+            if request.form['username'] == "":
+                error = 'Invalid username'
+            elif request.form['password'] == "":
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                flash('Logged in!')
+                return redirect(url_for('index'))
 
         return render_template('login.html', error=error)
 
