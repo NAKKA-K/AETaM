c6becae3d9a3bca22989d6dc910e5641f791f8ce
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-30 04:59:35 +0000

    #31 Add signup url

--- backend-aetam/aetam/urls.py
+++ backend-aetam/aetam/urls.py
@@ -3,3 +3,4 @@ from aetam import views
 
 app.add_url_rule('/', view_func=views.IndexView.as_view('index'))
 app.add_url_rule('/login', view_func=views.LoginView.as_view('login'))
+app.add_url_rule('/signup', view_func=views.SignUpView.as_view('signup'))
