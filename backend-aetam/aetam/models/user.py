2136317f7c2bd9e77e43047844639c37930effb1
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-31 03:50:09 +0000

    #31 Add user models

---
+++ backend-aetam/aetam/models/user.py
@@ -0,0 +1,21 @@
+class User(object):
+    @classmethod
+    def convert_dic(cls, user_array):
+        return dict(
+            id=user_array[0],
+            name=user_array[1]
+        )
+    @classmethod
+    def select_from(cls, db, user_id):
+        user = db.execute('select id, name from users where id=(?)', [user_id]).fetchone()
+        return cls.convert_dic(user)
+
+    def insert_to(self, db):
+        cursor = db.cursor()
+        cursor.execute('insert into users (name, password) values (?, ?)', [self.username, self.password])
+        db.commit()
+        return cursor.lastrowid
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
