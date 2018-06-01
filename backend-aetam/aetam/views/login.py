73afd88499a2f98b17bbd95f4962847eacf06747
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-01 14:20:06 +0000

    #31 Update response

--- backend-aetam/aetam/views/login.py
+++ backend-aetam/aetam/views/login.py
@@ -1,47 +1,33 @@
 from flask.views import MethodView
-from flask import render_template
-from flask import redirect
 from flask import request
-from flask import flash
-from flask import url_for
 from flask import session
 from flask import Response
 from flask import json
+from flask import abort
 
 
 class LoginView(MethodView):
-    def get(self):
-        return render_template('login.html')
-
     def post(self):
-        if request.headers['Content-Type'] == 'application/json':
-            data = {"errors": []}
-            if request.json['username'] == "":
-                data['errors'].append('Invalid username')
-            if request.json['password'] == "":
-                data['errors'].append('Invalid password')
+        data = {"errors": []}
+        if request.headers['Content-Type'] != 'application/json':
+            abort(406)
+            return
 
-            if data['errors'] == []:
-                session['logged_in'] = True
-                return Response(
-                    status=200,
-                    response=json.dumps(data),
-                    mimetype='application/json'
-                )
+        if request.json['username'] == "":
+            data['errors'].append('Invalid username')
+        if request.json['password'] == "":
+            data['errors'].append('Invalid password')
+        if data['errors'] == []:
+            session['logged_in'] = True
             return Response(
-                status=400,
+                status=200,
                 response=json.dumps(data),
                 mimetype='application/json'
             )
-        else: # TODO: When delelopment is completed, remove the implementation
-            if request.form['username'] == "":
-                error = 'Invalid username'
-            elif request.form['password'] == "":
-                error = 'Invalid password'
-            else:
-                session['logged_in'] = True
-                flash('Logged in!')
-                return redirect(url_for('index'))
 
-        return render_template('login.html', error=error)
+        return Response(
+            status=400,
+            response=json.dumps(data),
+            mimetype='application/json'
+        )
 
