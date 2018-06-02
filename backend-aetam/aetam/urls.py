6fb471fe18b61e5a7fb48ed7308db270c0c7faf3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-02 13:04:26 +0000

    #31 Add logout url

--- backend-aetam/aetam/urls.py
+++ backend-aetam/aetam/urls.py
@@ -3,4 +3,5 @@ from aetam import views
 
 app.add_url_rule('/', view_func=views.IndexView.as_view('index'))
 app.add_url_rule('/login', view_func=views.LoginView.as_view('login'))
+app.add_url_rule('/logout', view_func=views.LogoutView.as_view('logout'))
 app.add_url_rule('/signup', view_func=views.SignUpView.as_view('signup'))
