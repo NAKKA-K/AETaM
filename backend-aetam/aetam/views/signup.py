86394214fcc2cd751103cfa3fc2f1f8c5c772d82
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-02 16:38:37 +0000

    #31 Refactor decorator & jsonify

--- backend-aetam/aetam/views/signup.py
+++ backend-aetam/aetam/views/signup.py
@@ -3,34 +3,28 @@ from flask import request
 from flask import session
 from flask import Response
 from flask import json
+from flask import jsonify
 from flask import g
 from aetam import app
 from aetam.models import User
 from aetam.models import Status
+from aetam.views.util import content_type
 
 class SignUpView(MethodView):
+    @contetn_type('application/json')
     def post(self):
-        if request.headers['Content-Type'] == 'application/json':
-            data = {"errors": []}
-            if request.json['username'] == "":
-                data['errors'].append('Invalid username')
-            if request.json['password'] == "":
-                data['errors'].append('Invalid password')
+        data = {"errors": []}
+        if request.json['username'] == "":
+            data['errors'].append('Invalid username')
+        if request.json['password'] == "":
+            data['errors'].append('Invalid password')
 
-            if data['errors'] == []:
-                user_id = self.insert_user(request.json['username'], request.json['password'])
-                data['status'] = Status.select_from(g.db, user_id)
-                data['user'] = User.select_from(g.db, user_id)
-                return Response(
-                    status=200,
-                    response=json.dumps(data),
-                    mimetype='application/json'
-                )
-            return Response(
-                status=400,
-                response=json.dumps(data),
-                mimetype='application/json'
-            )
+        if data['errors'] == []:
+            user_id = self.insert_user(request.json['username'], request.json['password'])
+            data['status'] = Status.select_from(g.db, user_id)
+            data['user'] = User.select_from(g.db, user_id)
+            return jsonify(data), 200
+        return jsonify(data), 400
 
     def insert_user(self, username, password):
         pass_sha = self.SHA256_pass(password)
