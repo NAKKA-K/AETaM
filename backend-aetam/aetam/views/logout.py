a99739ebf508d2250cdbc54a636667b9fa5b8697
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-02 13:03:34 +0000

    #31 Add logout view

--- backend-aetam/aetam/views/logout.py
+++ backend-aetam/aetam/views/logout.py
@@ -1,8 +1,10 @@
 from flask.views import MethodView
 from flask import session
 from flask import abort
+from flask import jsonify
 
 class LogoutView(MethodView):
     def post(self):
+        data = {'errors': []}
         session.pop('logged_in', None)
-        abort(200)
+        return jsonify(data), 200
