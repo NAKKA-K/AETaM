2a477c79162543ae0409028c9cb7b798b68fbbd0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-03 12:31:38 +0000

    #31 Add login html page test

--- backend-aetam/aetam/tests/test_login.py
+++ backend-aetam/aetam/tests/test_login.py
@@ -7,27 +7,19 @@ class TestLogin(unittest.TestCase):
         self.app = app.test_client()
 
     def test_api_post_login(self):
-        response = self.api_post_login({
+        response = self.app.post('/login', data={
             'username': 'name',
-            'password': 'pass'
-        })
+            'password': 'password'
+        }, follow_redirects=True)
         self.assertEqual(200, response.status_code)
-        self.assertEqual([], response.get_json()['errors'])
 
     def test_api_post_login_failed(self):
-        response = self.api_post_login({
+        response = self.app.post('/login', data={
             'username': '',
             'password': ''
         })
         self.assertEqual(400, response.status_code)
-        self.assertIn('Invalid username', response.get_json()['errors'])
-
-    def api_post_login(self, data):
-        return self.app.post('/login',
-                data=json.dumps(data),
-                content_type='application/json')
 
     def test_api_post_logout(self):
         response = self.app.post('/logout')
         self.assertEqual(200, response.status_code)
-        self.assertEqual([], response.get_json()['errors'])
