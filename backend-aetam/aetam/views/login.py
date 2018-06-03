100dd460ff643028c9f6952350eeaf997fa4b4a8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-03 12:32:13 +0000

    #31 Add login html page

--- backend-aetam/aetam/views/login.py
+++ backend-aetam/aetam/views/login.py
@@ -1,23 +1,31 @@
 from flask.views import MethodView
 from flask import request
 from flask import session
-from flask import Response
-from flask import json
 from flask import jsonify
 from flask import abort
+from flask import render_template
+from aetam.models import User
 from aetam.views.util import content_type
 
 class LoginView(MethodView):
-    @content_type('application/json')
+    def get(self):
+        return render_template('login.html')
+
     def post(self):
         data = {"errors": []}
 
-        if request.json['username'] == "":
-            data['errors'].append('Invalid username')
-        if request.json['password'] == "":
-            data['errors'].append('Invalid password')
-        if data['errors'] == []:
-            session['logged_in'] = True
-            return jsonify(data)
+        for f in request.data:
+            print(f)
+            print('\n')
 
-        return jsonify(data), 400
+        if not User.is_valid_username(request.form['username']):
+            data['errors'].append('Invalid username')
+        if not User.is_valid_password(request.form['password']):
+            data['errors'].append('Invalid password')
+
+        if data['errors']:
+            return abort(400)
+
+        session['logged_in'] = True
+        return render_template('index.html', error=data['errors'])
+
