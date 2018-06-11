f0aa0dce97cfa4c6e90fd738b1320319b510d139
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-11 01:02:28 +0000

    #15 Add status API test

---
+++ backend-aetam/aetam/tests/test_status_api.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+from aetam import app
+from aetam.models import util_db
+from flask import json
+
+
+class TestStatusApi(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.set_test_db()
+
+    @classmethod
+    def set_test_db(cls):
+        app.config['DATABASE'] = os.path.join(app.config['BASE_DIR'], 'test.sqlite3')
+        util_db.init()
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        self.truncate_test_db_table()
+
+    def test_status_get_api(self):
+        access = self.app.post(
+            '/signup',
+            data=json.dumps({'username':'name', 'password':'password'}),
+            content_type='application/json'
+        ).get_json()['user']['access_key']
+        response = self.app.get(
+            '/statuses',
+            data=json.dumps({'access_key':access}),
+            content_type='application/json'
+        )
+        self.assertEqual(200, response.status_code)
+        if response.get_json()['errors'] != []:
+            self.fail()
+
+    def truncate_test_db_table(self):
+        with util_db.connect() as db:
+            db.execute('delete from Users')
+            db.execute('delete from Statuses')
